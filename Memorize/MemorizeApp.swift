41900aa00b074c4c4cfbcf19dcd36eb966d63ce2
moyales <moyales@stevens.edu>
2022-01-06 15:59:27 -0500

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Matthew Oyales on 1/6/22.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
